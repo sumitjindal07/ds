9f498e4300d530194d54978b70da5e768fcae66e
sumitjindal07 <14348347+sumitjindal07@users.noreply.github.com>
2020-01-19 00:25:52 +0530

    Add Serialization to break Singleton

--- DesignPatterns/src/com/sj/pattern/singleton/SingletonMain.java
+++ DesignPatterns/src/com/sj/pattern/singleton/SingletonMain.java
@@ -1,5 +1,11 @@
 package com.sj.pattern.singleton;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,8 +31,42 @@ public class SingletonMain {
 		}
 		
 		System.out.println("main-Count of singletonSet="+singletonSet.size());
+		
+		Singleton ins = singletonSet.toArray(new Singleton[1])[0];
+		ins.printInstanceAndIncrement();
+		
+		try {
+			serializeSingleton(ins);
+			Singleton deserializeIns = deserializeSingleton();
+			
+			if(ins!=deserializeIns) {
+				System.out.println("another instance of Singleton is created");
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
+	private static Singleton deserializeSingleton() throws IOException, ClassNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("tmp"));
+		Object obj = ois.readObject();
+		ois.close();
+		return (Singleton) obj;
+	}
+
+	private static void serializeSingleton(Singleton ins) throws IOException {
+		FileOutputStream bos = new FileOutputStream("tmp");
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(ins);
+		oos.close();
+		if(new File("tmp").exists()) {
+			System.out.println("Serialization of "+ins+" is done!!!");
+		} else {
+			System.out.println("Serialization of "+ins+" is NOT done!!!");
+		}
+	}
+
 	static class CreateSingleton implements Runnable {
 		
 		Set<Singleton> singletonSet = null;
